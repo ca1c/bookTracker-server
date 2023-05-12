dc04c5b73824052002269faec7c31e85caaa2b65
ca1c <gegs921@gmail.com>
2023-05-11 17:46:32 -0700

    max search results

--- router/routes/public/googleBooks.js
+++ router/routes/public/googleBooks.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 async function searchBook(req, res) {
 
 	try {
-		let response = await axios.get(`${process.env.API_URL}?q=${req.query.q}&key=${process.env.API_KEY}`);
+		let response = await axios.get(`${process.env.API_URL}?q=${req.query.q}&maxResults=40&key=${process.env.API_KEY}`);
 		res.send(response.data);
 	} catch(error) {
 		console.log(error);
